c150a86eea9db7192a92df2b21e23115a67ff235
Mohamed Nafih <102818533+mdnafihkt@users.noreply.github.com>
2026-04-30 11:10:15 +0530

    chore: harden release build security settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,10 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
